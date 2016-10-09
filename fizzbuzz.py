bc1db689067d512b268a6342a79377b7ea9804dd
Otyty 7 <Otyty 7>
2016-10-09 10:27:19 +0300

    Adding My Labs

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizz_buzz(num):
+  if ((num % 3 == 0) and (num % 5 != 0)):
+    return 'Fizz'
+  
+  elif ((num % 5 == 0) and (num % 3 != 0)):
+    return 'Buzz'
+    
+  elif ((num % 3 == 0) and (num % 5 == 0)):
+    return 'FizzBuzz'
+ 
+  return num
